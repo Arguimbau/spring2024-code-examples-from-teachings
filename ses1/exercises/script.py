ac93cb81e22e0c4ff7933d84b7573eb99b39efbb
ArthurOS <arth3608@stud.kea.dk>
2024-01-29 10:13:54 +0100

    intro

--- ses1/exercises/script.py
+++ ses1/exercises/script.py
@@ -1,3 +1,11 @@
-x = 12
-x
-print(x)
+def message(name):
+    a = 'Claus'
+    if name == a.lower():
+
+        print("Hello World!" + str(name))
+
+    else:
+
+        print("No no no")
+
+message('claus')
